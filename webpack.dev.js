76933ff0b3575999ae1f20dbdee2a0806c438ec4
Ayatullah Khamini <ayat2486@gmail.com>
2020-05-12 19:50:00 +0600

    sass configure running

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,13 +7,23 @@ module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
-    stats: 'verbose',
+    devServer: {
+        port: 3000
+    },
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
